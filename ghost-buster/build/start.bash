c5b3590e825b6fecb39af74e5258d9f66e160050
ZuoLan <i@zuolan.me>
2016-06-03 08:45:58 +0800

    Update start.bash

--- ghost-buster/build/start.bash
+++ ghost-buster/build/start.bash
@@ -4,11 +4,12 @@ GHOST="/ghost"
 
 sed -i -e "s|http://my-ghost-blog.com|${BLOG_DOMAIN}|g" /ghost/config.js
 
+mkdir ${GHOST}/content/data/
 #start background watcher for buster
 while inotifywait -e modify /ghost/content/data/; do sleep 10 && /ghost/publish.sh; done &
 
 # Start Ghost
-chown -R ghost:ghost /data /ghost
+chown -R ghost:ghost /ghost
 su ghost << EOF
 cd "$GHOST"
 NODE_ENV=${NODE_ENV:-production} npm start
