b73dfdf12bf933e43af5bbd69a7c87ce4a11918f
ZuoLan <i@zuolan.me>
2016-06-03 08:23:14 +0800

    Update start.bash

--- ghost-buster/build/start.bash
+++ ghost-buster/build/start.bash
@@ -5,7 +5,7 @@ GHOST="/ghost"
 sed -i -e "s|http://my-ghost-blog.com|${BLOG_DOMAIN}|g" /ghost/config.js
 
 #start background watcher for buster
-while inotifywait -e modify /ghost/content/data/ghost.db; do sleep 10 && /ghost/publish.sh; done &
+while inotifywait -e modify /ghost/content/data/; do sleep 10 && /ghost/publish.sh; done &
 
 # Start Ghost
 chown -R ghost:ghost /data /ghost
