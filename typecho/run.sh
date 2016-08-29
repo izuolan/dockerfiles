18f63649257b57ec951ba40418ec436bdc37adce
ZuoLan <i@zuolan.me>
2016-08-29 15:23:04 +0800

    Update run.sh

--- typecho/run.sh
+++ typecho/run.sh
@@ -7,7 +7,7 @@ else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
     a2enmod rewrite
 fi
-
+a2enmod ssl
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
