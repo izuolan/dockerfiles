59456de2038919e62f4a55929dfc21cf05aea134
izuolan <i@zuolan.me>
2017-02-21 14:27:16 +0000

    fix hexo

--- hexo/monitor.sh
+++ hexo/monitor.sh
@@ -2,31 +2,33 @@
 SEPARATOR="================================================================"
 echo "正在执行初次构建："
 echo $SEPARATOR
-hexo g
-chmod 777 -R /hexo/public
-echo -e "\n\n"
-echo "页面构建完成，正在发布到 Github："
-echo $SEPARATOR
+if [ "$COMPRESS" = "true" ]; then
+    echo "正在压缩图片和中文字体："
+    chroot /host_dir docker run --rm -v $HEXO/themes:/html -v $HEXO/source:/images zuolan/font-spider
+fi
+echo "正在构建并部署页面："
 ssh -o "StrictHostKeyChecking no" -T git@github.com
-hexo d
-echo -e "\n\n"
+hexo d -g
+chmod 777 -R /hexo/public
 echo "页面已经发布，容器进入监视状态。"
 VOLUMES="source themes _config.yml"
 INOTIFY_EVENTS="create,delete,modify,move"
-INOTIFY_OPTONS="--monitor --exclude=(\.sw[p|x]|\~)"
+INOTIFY_OPTONS="--monitor --exclude=(\.sw[p|x]|\~|\.part)"
 inotifywait -rqe ${INOTIFY_EVENTS} ${INOTIFY_OPTONS} ${VOLUMES} | \
     while read -r notifies;
     do
-    	echo -e "\n\n"
         echo $SEPARATOR
         echo "文件有变动："
         echo "$notifies"
-        echo -e "\n\n"
         echo "正在执行重新构建并发布："
         echo $SEPARATOR
+        if [ "$COMPRESS" = "true" ]; then
+            echo "正在压缩图片和中文字体："
+            chroot /host_dir docker run --rm -v $HEXO/themes:/html -v $HEXO/source:/images zuolan/font-spider
+        fi
+        echo "正在构建并部署页面："
         hexo d -g
         chmod 777 -R /hexo/public
-        echo -e "\n\n"
         echo "新的页面构建完成。"
         echo $SEPARATOR
     done
