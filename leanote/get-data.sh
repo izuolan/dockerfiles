259cc3389406e16edaab8e23149798420e611ef3
izuolan <i@zuolan.me>
2018-01-22 14:30:17 +0800

    Update Leanote version

--- leanote/get-data.sh
+++ leanote/get-data.sh
@@ -1,5 +1,5 @@
 #! bin/sh
-LEANOTE_VERSION=2.3
+LEANOTE_VERSION=2.6
 wget https://jaist.dl.sourceforge.net/project/leanote-bin/${LEANOTE_VERSION}/leanote-linux-amd64-v${LEANOTE_VERSION}.bin.tar.gz
 tar zxf leanote-linux-amd64-v${LEANOTE_VERSION}.bin.tar.gz
 mv leanote/mongodb_backup .
--- leanote/initdb.sh
+++ leanote/initdb.sh
@@ -1,2 +1,4 @@
 #! bin/sh
+echo "Loading...."
+sleep 5
 mongorestore -h mongo -d leanote --dir /mongodb_backup/leanote_install_data/
