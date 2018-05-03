c2b47a98f82c25e13f70bdcfa5df6d1f416b6704
root <root@zuolan-dev.c.zuo-lan.internal>
2018-05-03 09:05:49 +0000

    Hexo fixed

--- hexo/monitor.sh
+++ hexo/monitor.sh
@@ -2,14 +2,14 @@
 if [ ! -f "/hexo/_config.yml" ]; then
   echo "Monitor: _config.yml not found, runing hexo init"
   hexo init .
-  yarn add hexo-deployer-git hexo-deployer-shell hexo-neat
-  yarn install
 fi
 SEPARATOR="================================================================"
 echo "正在执行初次构建："
 echo $SEPARATOR
 echo "正在构建并部署页面："
 npm install hexo --save
+yarn add hexo-deployer-git hexo-deployer-shell hexo-neat
+yarn install
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
 ssh -o "StrictHostKeyChecking no" -T git@github.com
